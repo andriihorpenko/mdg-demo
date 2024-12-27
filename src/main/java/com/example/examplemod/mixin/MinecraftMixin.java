2b9ad7e04647685ef5b254111f87be7afe42e7bd
andriihorpenko <andrii.horpenko@gmail.com>
2024-12-27 11:35:43 +0200

    Mixins refmap is only applied to "jar" task

--- src/main/java/com/example/examplemod/mixin/MinecraftMixin.java
+++ src/main/java/com/example/examplemod/mixin/MinecraftMixin.java
@@ -2,7 +2,14 @@ package com.example.examplemod.mixin;
 
 import net.minecraft.client.Minecraft;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(Minecraft.class)
-public class MinecraftMixin {
+public abstract class MinecraftMixin {
+    @Inject(method = "close", at = @At("HEAD"))
+    private void demo(CallbackInfo ci) {
+        System.out.println(((Minecraft) (Object) this).cameraEntity.blocksBuilding);
+    }
 }
