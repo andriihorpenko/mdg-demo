b1db7fc716dabaeb0acde4da8180c648e77009fc
andriihorpenko <andrii.horpenko@gmail.com>
2024-12-27 11:24:12 +0200

    Init

---
+++ src/main/java/com/example/examplemod/ExampleMod.java
@@ -0,0 +1,8 @@
+package com.example.examplemod;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(ExampleMod.MODID)
+public class ExampleMod {
+    public static final String MODID = "examplemod";
+}
